d5654ee8130a1dda03e7955c098d98a895fea7cc
yunyun-c <82518667+yunyun-c@users.noreply.github.com>
2021-07-25 23:18:40 +0800

    Select Elements & Add Placeholders

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const guessedLetter = document.querySelector(".guessed-letters");
+const hintMessage = document.querySelector(".message");
+const remainMessage = document.querySelector(".remaining")
+const numOfremainig = document.querySelector(".remaining span");
+const wordInProgress = document.querySelector(".word-in-progress");
+const letterInput = document.querySelector(".letter");
+const guessLetterButton = document.querySelector(".guess")
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const placeholder = function(word){
+    const placeholderLetters = [];
+    for (let letter of word){
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function(e){
+    e.preventDefault();
+    const guess = letterInput.nodeValue;
+    console.log(guess);
+    letterInput.value = "";
+});
